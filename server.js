b5ed884b688f2300ba888ee0fd38bcbda6303a75
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-07 18:30:42 +0300

    .

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3000;
-
+// Serve static assets (CSS, JS, images)
 app.use("/images", express.static(path.join(__dirname, "images")));
+// Routes for any html pages created 
 app.use('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"index.html"))
     console.log("request is served by the app")
