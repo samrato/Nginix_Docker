4c522f228787e8fb5d3193c5c643733549961b83
Willington Juma <willingtonejumamconyango@gmail.com>
2025-05-08 21:45:54 +0300

    .

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = 3000;
+
+const replicaapp=process.env.APP_NAME
 // Serve static assets (CSS, JS, images)
 app.use("/images", express.static(path.join(__dirname, "images")));
 // Routes for any html pages created 
@@ -9,7 +11,7 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"index.html"))
-    console.log("request is served by the app")
+    console.log(`request is served by ${replicaapp}`)
 })
 
 // another html could have been like this  okay
